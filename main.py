7989ad28368728b733a7506a086b122b0ff54e83
Debajyoti28 <mrigankaghosh19@gmail.com>
2025-03-16 12:38:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Original Image", use_column_width=True)
+    st.image(image, caption="Original Image", use_container_width=True)
 
     # Resize options
     width = st.number_input("Enter new width", min_value=1, value=image.width)
@@ -16,7 +16,7 @@ if uploaded_file is not None:
 
     if st.button("Resize Image"):
         resized_image = image.resize((width, height))
-        st.image(resized_image, caption="Resized Image", use_column_width=True)
+        st.image(resized_image, caption="Resized Image", use_container_width=True)
 
         # Convert image to bytes for download
         img_format = uploaded_file.name.split(".")[-1].upper()
